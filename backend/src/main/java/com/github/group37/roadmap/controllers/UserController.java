e670c80ec3354847c2fdd8fa42086ec34c666aec
renasustek <renasustek@gmail.com>
2023-11-09 18:04:36 +0000

    Controller layer for the user is completed, going to stat unit testing

--- backend/src/main/java/com/github/group37/roadmap/controllers/UserController.java
+++ backend/src/main/java/com/github/group37/roadmap/controllers/UserController.java
@@ -1,11 +1,9 @@
 package com.github.group37.roadmap.controllers;
 
 import com.github.group37.roadmap.Models.User;
+import com.github.group37.roadmap.Models.UserRequest;
 import com.github.group37.roadmap.service.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -19,14 +17,25 @@ public class UserController {
         this.userService = userService;
     }
 
+    @PostMapping("/")
+    public User postUser(@RequestBody UserRequest userRequest){
+        User user = new User(UUID.randomUUID(), userRequest.getName(), userRequest.getPassword());
+        return userService.createUser(user);
+    }
+
     @GetMapping("/")
     public List<User> getAllUsers(){
         return userService.readUser();
     }
 
-    @PostMapping("/")
-    public User postUser(@RequestBody User user){
-        return userService.createUser(UUID.randomUUID(), );
+    @PutMapping("/")
+    public User updateUser(UUID id, @RequestBody UserRequest updatedUserDetails){
+        return userService.updateUser(id, updatedUserDetails.getName(),updatedUserDetails.getPassword());
+    }
+
+    @DeleteMapping("/")
+    public void deleteUser(UUID id){
+        userService.deleteUser(id);
     }
 
 }
