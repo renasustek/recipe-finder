f2e8dd6d86914cf5ff7a99ec49100f7e00fb58b5
renasustek <renasustek@gmail.com>
2024-02-09 23:34:40 +0000

    writing tests

--- backend/src/test/java/com/github/group37/roadmap/percistanceTest/UserRepositoryTest.java
+++ backend/src/test/java/com/github/group37/roadmap/percistanceTest/UserRepositoryTest.java
@@ -1,21 +1,19 @@
 package com.github.group37.roadmap.percistanceTest;
 
-import com.github.group37.roadmap.percistance.models.User;
 import com.github.group37.roadmap.percistance.UserRepository;
+import com.github.group37.roadmap.percistance.models.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 //@ExtendWith(SpringExtension.class)
 
