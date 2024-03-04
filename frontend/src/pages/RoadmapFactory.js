dc6fc2d3605a2a1d294c4c2e3d328fbac888be8e
renasustek <renasustek@gmail.com>
2024-03-04 22:55:46 +0000

    Page now loads roadmap if a user has one already, if user doesnt user is prompted to "create roadmap"

--- frontend/src/pages/RoadmapFactory.js
+++ frontend/src/pages/RoadmapFactory.js
@@ -1,14 +1,53 @@
-import React from  'react';
-
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import "../css/RoadmapFactory.css"
+import GetRoadmap from '../components/GetRoadmap';
 
 function RoadmapFactory({username, password}) {
+    const [roadmap, setRoadmap] = React.useState(null);
+
+
+    useEffect(() => {
+      // Replace the URL with the correct API endpoint
+      const apiUrl = `http://localhost:8080/roadmap/${username}`;
     
-
-
+      // Encode credentials in Base64
+      const credentials = btoa(`${username}:${password}`);
+      const basicAuth = `Basic ${credentials}`;
+  
+      axios.get(apiUrl, {
+        headers: {
+          Authorization: basicAuth,
+        },
+      })
+        .then((response) => {
+          setRoadmap(response.data);
+        })
+        .catch((error) => {
+          console.error('Error fetching data:', error);
+          setRoadmap(false)
+        });
+    }, []);
+    
+    
+   
+      console.log(roadmap)
+  
     return(
       <div>
-        <div>Hi {username}, welcome to your roadmap factory.</div>
-        <div>Put that other stuff here</div>
+        <div className='sideMenu'>
+          <div>Hi {username}, welcome to your roadmap factory.</div>
+          <div>View all avaliable subjects and topics</div>
+          <div>Generate Roadmap</div>
+        </div>
+        
+        
+        <div>
+         {roadmap ? <GetRoadmap roadmap={roadmap}/> : <div>create roadmap</div>}
+          
+        </div>
+      
+      
       </div>
     );
 
