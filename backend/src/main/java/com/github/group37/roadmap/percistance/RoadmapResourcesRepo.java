6879987e29b4d8590324bca52bf3112b0fb7e40c
renasustek <renasustek@gmail.com>
2024-03-11 20:18:36 +0000

    delete roadmap, deleting all instances of a roadmap with its specified id from table

--- backend/src/main/java/com/github/group37/roadmap/percistance/RoadmapResourcesRepo.java
+++ backend/src/main/java/com/github/group37/roadmap/percistance/RoadmapResourcesRepo.java
@@ -2,8 +2,10 @@ package com.github.group37.roadmap.percistance;
 
 import com.github.group37.roadmap.percistance.models.RoadmapResources;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
@@ -14,6 +16,8 @@ public interface RoadmapResourcesRepo extends JpaRepository<RoadmapResources, UU
     @Query("SELECT u.revisionResourceId FROM RoadmapResources u WHERE u.roadmapId = ?1")
     List<UUID> findAllResourcesUsingRoadmapId(UUID roadmapId);
 
+    @Transactional
+    @Modifying
     @Query("DELETE from RoadmapResources u where u.roadmapId = ?1")
     void deleteByRoadmapId(UUID roadmapId);
 }
